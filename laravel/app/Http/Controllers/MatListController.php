1e8c972e41015a9b578783ee6d4da218f19b40cb
Simon Tolfree <56439044+simontolfree@users.noreply.github.com>
2022-06-28 15:40:17 +0100

    Added controller for list of mats

---
+++ laravel/app/Http/Controllers/MatListController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MatList;
+use Illuminate\Http\Request;
+
+class MatListController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\MatList  $matList
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MatList $matList)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\MatList  $matList
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MatList $matList)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\MatList  $matList
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MatList $matList)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\MatList  $matList
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MatList $matList)
+    {
+        //
+    }
+}
