f6cf445f49fe3e0b7a25659df4bba9c3832da5ef
Simon Tolfree <56439044+simontolfree@users.noreply.github.com>
2022-07-02 15:38:18 +0100

    Added controller for list of products at events

---
+++ laravel/app/Http/Controllers/EventProductsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\EventProducts;
+use Illuminate\Http\Request;
+
+class EventProductsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\EventProducts  $eventProducts
+     * @return \Illuminate\Http\Response
+     */
+    public function show(EventProducts $eventProducts)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\EventProducts  $eventProducts
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(EventProducts $eventProducts)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\EventProducts  $eventProducts
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, EventProducts $eventProducts)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\EventProducts  $eventProducts
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(EventProducts $eventProducts)
+    {
+        //
+    }
+}
