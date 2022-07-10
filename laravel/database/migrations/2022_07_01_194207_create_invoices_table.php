f749a1ba50c32867dd843853bf2ba0a6c1303c8e
Simon Tolfree <56439044+simontolfree@users.noreply.github.com>
2022-07-10 19:16:54 +0100

    Added definition of invoices table

---
+++ laravel/database/migrations/2022_07_01_194207_create_invoices_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInvoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id('invoices_id');
+            $table->string('invoice_file_location')->nullable();
+            $table->string('place_of_purchase')->nullable();
+            $table->integer('invoice_cost')->nullable();
+            $table->integer('vat_cost')->nullable();
+            $table->timestamps();
+
+
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+}
