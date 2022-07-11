b35b9b70e39d22bf4c47800874d191f7e60b8446
Simon Tolfree <56439044+simontolfree@users.noreply.github.com>
2022-07-11 19:15:13 +0100

    Added definition of sale details table

---
+++ laravel/database/migrations/2022_06_28_185109_create_sales_details_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales_details', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('price');
+            $table->integer('inventory');
+            $table->string('user_sold');
+            $table->string('external_sale');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales_details');
+    }
+}
